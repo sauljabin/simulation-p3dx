264fd74ff9a2aaadaa1b00e909357abe2d1725b8
Saúl Piña <sauljabin@gmail.com>
2014-02-14 16:08:15 -0530

    Update reactive behavior

--- src/simulation/architecture/Reactive.java
+++ src/simulation/architecture/Reactive.java
@@ -1,13 +1,34 @@
 package simulation.architecture;
 
 import coppelia.remoteApi;
+import simulation.behavior.Avoid;
+import simulation.behavior.Escape;
+import simulation.behavior.Move;
 import simulation.interfaces.Architecture;
+import simulation.interfaces.Behavior;
 
 public class Reactive implements Architecture {
 
-	@Override
-	public void simulate(remoteApi vrep, int clientId) {
+	private Behavior avoid;
+	private Behavior escape;
+	private Behavior move;
 
+	public Reactive() {
+		avoid = new Avoid();
+		escape = new Escape();
+		move = new Move();
 	}
 
+	@Override
+	public void simulate(remoteApi vrep, int clientId) {
+		boolean bumper = true;
+		boolean sonar = true;
+
+		if (bumper)
+			escape.run(vrep, clientId);
+		else if (sonar)
+			avoid.run(vrep, clientId);
+		else if (!sonar)
+			move.run(vrep, clientId);
+	}
 }
--- src/simulation/behavior/Avoid.java
+++ src/simulation/behavior/Avoid.java
@@ -1,7 +1,13 @@
 package simulation.behavior;
 
+import coppelia.remoteApi;
 import simulation.interfaces.Behavior;
 
 public class Avoid implements Behavior {
+
+	@Override
+	public void run(remoteApi vrep, int clientId) {
+		
+	}
 
 }
